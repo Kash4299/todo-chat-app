41c5b55d1bedf3a4a4648b71a1c066335172d507
Kash <apple@Apples-MacBook-Pro.local>
2026-02-18 21:25:18 +0700

    update

--- internal/repositories/init.go
+++ internal/repositories/init.go
@@ -8,11 +8,13 @@ import (
 	"go.uber.org/fx"
 )
 
-// Module provides all repository dependencies
+// Module provides all repository dependencies.
+// fx.As() tells Fx that concrete types satisfy the corresponding interfaces,
+// so that Hub (which depends on interfaces) can receive them.
 var Module = fx.Options(
 	fx.Provide(
-		todo.NewTodoRepository,
-		user.NewUserRepository,
-		chat.NewChatRepository,
+		fx.Annotate(todo.NewTodoRepository, fx.As(new(todo.ITodoRepository))),
+		fx.Annotate(user.NewUserRepository, fx.As(new(user.IUserRepository))),
+		fx.Annotate(chat.NewChatRepository, fx.As(new(chat.IChatRepository))),
 	),
 )
