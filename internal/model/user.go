20be6e3fb8fc9409871a51ea06dc87b11bb1c5b7
Kash <apple@MacBookPro.lan>
2026-04-12 18:39:18 +0700

    remove all

--- internal/model/user.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type User struct {
-	BaseModel
-
-	Username string `gorm:"column:username" json:"username"`
-	Password string `gorm:"column:password" json:"password"`
-	Email    string `gorm:"column:email" json:"email"`
-}
